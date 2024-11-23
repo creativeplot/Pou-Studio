866e03db5df1aa96acaa5d8668222d4a4e19ba33
creativeplot <victor.gamer1@hotmail.com>
2024-11-23 09:18:55 -0300

    fix-images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,10 @@
 
-import pinguins from "../assets/images/pinguins-at-the-beach.jpg";
+import pinguins from "../assets/images/pinguins-at-the-beach.jpg"
 import dogWithGlasses from "../src/assets/images/dog-with-glasses.jpg"
+import catWithGlasses from "../src/assets/images/cat-with-glasses.avif"
+import pandEating from "../src/assets/images/panda-eating.jpg"
+import gorilaThinking from "../src/assets/images/gorila-thinking.jpg"
+import owl from "../src/assets/images/owl-looking-at-you.webp" 
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 
@@ -32,16 +36,16 @@ const Main = () => {
             <img src={pinguins} alt="pinguins-at-the-beach" className="rounded-xl custom-image custom-h"/>
         </figure>
         <figure className="rounded-xl w-full flex justify-center items-center custom-h">
-            <img src="../src/assets/images/cat-with-glasses.avif" alt="cat-with-glasses" className="rounded-xl custom-image custom-h"/>
+            <img src={catWithGlasses} alt="cat-with-glasses" className="rounded-xl custom-image custom-h"/>
         </figure>
         <figure className="rounded-xl w-full flex justify-center items-center custom-h">
-            <img src="../src/assets/images/panda-eating.jpg" alt="panda-eating " className="rounded-xl w-full h-full custom-image"/>
+            <img src={pandEating} alt="panda-eating " className="rounded-xl w-full h-full custom-image"/>
         </figure>
         <figure className="rounded-xl w-full flex justify-center items-center custom-h">
-            <img src="../src/assets/images/gorila-thinking.jpg" alt="gorila-thinking" className="rounded-xl custom-image"/>
+            <img src={gorilaThinking} alt="gorila-thinking" className="rounded-xl custom-image"/>
         </figure>
         <figure className="rounded-xl w-full flex justify-center items-center custom-h">
-            <img src="../src/assets/images/owl-looking-at-you.webp" alt="owl-looking-at-you" className="rounded-xl custom-image"/>
+            <img src={owl} alt="owl-looking-at-you" className="rounded-xl custom-image"/>
         </figure>
 
     </motion.main>
